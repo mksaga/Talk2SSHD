d283ac0354a4a462e481980182cccd8e42bff71f
Mohamed Aly <mohamed.aly06@gmail.com>
2019-06-14 12:04:44 -0500

    Begins work on porting serial port code to GUI
    Sets left config part of screen to max out at width 450

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <stdlib.h>
+
+#include <QByteArray>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QString>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Set up necessary data
+    uint8_t Crc8Table;
+    uint16_t sensorId;
+    QByteArray message;
+    QString errorString;
+
+    // set up serial port (decl. in header file)
+    port = new QSerialPort();
 }
 
 MainWindow::~MainWindow()
 {
+    if (port->isOpen()) {
+        port->close();
+    }
+    serialThread->exit();
+    serialThread->wait();
+
     delete ui;
+    delete port;
+    delete serialThread;
+}
+
+
+// Updates the list of available serial port devices
+void MainWindow::on_refreshComPorts_clicked()
+{
+    ui->comPortSelect->clear();
+    // iterate over all available ports
+    foreach(QSerialPortInfo p, QSerialPortInfo::availablePorts())
+    {
+        ui->comPortSelect->addItem(QString(p.portName() + " (" +
+                                           p.description() + ")"));
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QThread>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_refreshComPorts_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSerialPort *port;
+    QThread *serialThread;
 };
 
 #endif // MAINWINDOW_H
